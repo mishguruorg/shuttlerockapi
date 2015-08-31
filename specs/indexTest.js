e291ea103c0c364b7e71038ff00537b1d7b704ff
Jacob Duval <jladuval@gmail.com>
2015-08-31 17:11:49 +1200

    Added new init test

--- specs/indexTest.js
+++ specs/indexTest.js
@@ -1,9 +1,19 @@
 /* globals describe, it */
 import {expect} from 'chai'
 import * as api from '../lib/'
+import * as settings from '../lib/config/settings'
 
 describe('When I initialise the library', () => {
   it('It should throw an error if I do not input any options', () => {
     expect(api.init).to.throw(Error)
+  })
+
+  it('It should change add appropriate fields to settings when they are passed in', () => {
+    let accountName = 'mishGuru'
+    api.init({
+      accountName
+    })
+
+    expect(settings.hostname).to.be.eq(`${accountName}.shuttlerock.com`)
   })
 })
